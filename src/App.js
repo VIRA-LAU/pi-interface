dbd76549ce187d444d1f5fba810cc2f44bab0a76
Roland Salloum <roland.salloum00@outlook.com>
2022-03-16 22:41:23 +0200

    feat: add buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
+import Button from '@mui/material/Button';
+
+
 const Cam = () => {
     return (
         <div>
-            <img
-                src="http://localhost:5000/video_feed"
-                alt="Video"
-            />
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <img
+                    src="http://localhost:5000/video_feed"
+                    alt="Video"
+                />
+            </div>
+            <div style={{display: "flex", justifyContent: "center"}}>Control</div>
+            <Button variant="text">Text</Button>
         </div>
     );
 };
