611bc9e1c2de9849d0148b9e6a0f2797346e1ce4
Roland Salloum <roland.salloum00@outlook.com>
2022-03-16 23:43:52 +0200

    feat: keyboard buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Button from '@mui/material/Button';
+
+export const FORWARD = "FORWARD";
+export const BACKWARD = "BACKWARD";
+export const LEFT = "LEFT";
+export const RIGHT = "RIGHT";
 
 
 const Cam = () => {
+    const [status,setStatus] = useState("")
+    const intervalRef = useRef(null)
+
+    useEffect(() => {
+        console.log(status)
+        switch (status){
+            case FORWARD:
+                intervalRef.current = setInterval(()=>{
+                    console.log("Send Forward Request")
+                },500);
+                break;
+            case LEFT:
+                intervalRef.current = setInterval(()=>{
+                    console.log("Send Left Request")
+                },500);
+                break;
+            case RIGHT:
+                intervalRef.current = setInterval(()=>{
+                    console.log("Send right Request")
+                },500);
+                break;
+            case BACKWARD:
+                intervalRef.current = setInterval(()=>{
+                    console.log("Send backward Request")
+                },500);
+                break;
+            default:
+                clearInterval(intervalRef.current)
+                intervalRef.current = null
+
+        }
+    },[status])
+
     return (
         <div>
             <div style={{display: "flex", justifyContent: "center"}}>
@@ -12,7 +50,48 @@ const Cam = () => {
                 />
             </div>
             <div style={{display: "flex", justifyContent: "center"}}>Control</div>
-            <Button variant="text">Text</Button>
+            <div style={{
+                display:'flex',
+                placeContent:'center'
+            }}>
+                <div>
+                    <div style={{
+                        display:'flex',
+                        placeContent:'center'
+                    }}>
+                        <Button variant="text"
+                                onMouseDown={()=> setStatus(FORWARD)}
+                                onMouseUp={() => setStatus("")}
+                                onTouchStart={()=> setStatus(FORWARD)}
+                                onTouchEnd={() => setStatus("")}
+                                onTouchCancel={() => setStatus("")}
+                        >F</Button>
+                    </div>
+                    <div style={{display:'flex'}}>
+                        <Button variant="text"
+                                onMouseDown={()=> setStatus(LEFT)}
+                                onMouseUp={() => setStatus("")}
+                                onTouchStart={()=> setStatus(LEFT)}
+                                onTouchEnd={() => setStatus("")}
+                                onTouchCancel={() => setStatus("")}
+                        >L</Button>
+                        <Button variant="text"
+                                onMouseDown={()=> setStatus(BACKWARD)}
+                                onMouseUp={() => setStatus("")}
+                                onTouchStart={()=> setStatus(BACKWARD)}
+                                onTouchEnd={() => setStatus("")}
+                                onTouchCancel={() => setStatus("")}
+                        >B</Button>
+                        <Button variant="text"
+                                onMouseDown={()=> setStatus(RIGHT)}
+                                onMouseUp={() => setStatus("")}
+                                onTouchStart={()=> setStatus(RIGHT)}
+                                onTouchEnd={() => setStatus("")}
+                                onTouchCancel={() => setStatus("")}
+                        >R</Button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
